e97330eeea8274ce2f844e69250f01af3cab37ea
wang-ux888 <wangdao1988@gmail.com>
2026-07-09 01:22:12 +0800

    Add files via upload

---
+++ zqoffice_twc/src/version.cpp
@@ -0,0 +1,21 @@
+﻿// =============================================================================
+// src/version.cpp
+//
+// 实现 version.h 中的版本字符串接口
+// =============================================================================
+#include "zq/office/version.h"
+
+#include <string>
+
+namespace zq {
+namespace office {
+
+const char* versionString() {
+    static const std::string s = std::string("ZQOffice ")
+        + kVersionString
+        + " (based on ZQOffice)";
+    return s.c_str();
+}
+
+} // namespace office
+} // namespace zq
